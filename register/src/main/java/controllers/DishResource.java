400b6bc506df919c3f128a0e5c1c13e489ad2e18
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-17 10:52:42 -0300

    dishes CRUD based on restaurants

--- register/src/main/java/controllers/DishResource.java
+++ register/src/main/java/controllers/DishResource.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import entities.Dish;
+import entities.Restaurant;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
@@ -9,30 +10,49 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Optional;
 
-@Path("/dishes")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class DishResource {
 
     @GET
-    public List<Dish> search() {
-        return Dish.listAll();
+    @Path("{idRestaurant}/dishes")
+    public List<Dish> search(@PathParam("idRestaurant") Long idRestaurant) {
+        Optional<Restaurant> restaurantOp = Restaurant.findByIdOptional(idRestaurant);
+        if (restaurantOp.isEmpty()) {
+            throw new NotFoundException("Restaurant do not exist");
+        }
+        return Dish.list("restaurant", restaurantOp.get());
     }
 
     @POST
+    @Path("{idRestaurant}/dishes")
     @Transactional
-    public Response add(Dish dto) {
-        dto.persist();
+    public Response add(@PathParam("idRestaurant") Long idRestaurant, Dish dto) {
+        Optional<Restaurant> restaurantOp = Restaurant.findByIdOptional(idRestaurant);
+        if (restaurantOp.isEmpty()) {
+            throw new NotFoundException("Restaurant do not exist");
+        }
+        Dish dish = new Dish();
+        dish.name = dto.name;
+        dish.description = dto.description;
+        dish.price = dto.price;
+        dish.restaurant = restaurantOp.get();
+        dish.persist();
         return Response.status(Response.Status.CREATED).build();
     }
 
     @PUT
-    @Path("{id}")
+    @Path("{idRestaurant}/dishes/{id}")
     @Transactional
-    public void update(@PathParam("id") Long id, Dish dto) {
+    public void update(@PathParam("idRestaurant") Long idRestaurant, @PathParam("id") Long id, Dish dto) {
+        Optional<Restaurant> restaurantOp = Restaurant.findByIdOptional(idRestaurant);
+        if (restaurantOp.isEmpty()) {
+            throw new NotFoundException("Restaurant do not exist");
+        }
+
         Optional<Dish> dishOp = Dish.findByIdOptional(id);
         if(dishOp.isEmpty()) {
-            throw new NotFoundException();
+            throw new NotFoundException("Dish do not exist");
         }
         Dish dish = dishOp.get();
         dish.name = dto.name;
@@ -40,11 +60,15 @@ public class DishResource {
     }
 
     @DELETE
-    @Path("{id}")
+    @Path("{idRestaurant}/dishes/{id}")
     @Transactional
-    public void delete(@PathParam("id") Long id) {
-        Optional<Dish> dishOp = Dish.findByIdOptional(id);
+    public void delete(@PathParam("idRestaurant") Long idRestaurant, @PathParam("id") Long id) {
+        Optional<Restaurant> restaurantOp = Restaurant.findByIdOptional(idRestaurant);
+        if (restaurantOp.isEmpty()) {
+            throw new NotFoundException("Restaurant do not exist");
+        }
 
+        Optional<Dish> dishOp = Dish.findByIdOptional(id);
         dishOp.ifPresentOrElse(Dish::delete, () -> {
             throw new NotFoundException();
         });
