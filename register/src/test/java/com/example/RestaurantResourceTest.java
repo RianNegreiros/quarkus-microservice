75cf3aeb5dcbdb74d4c93b46f398baf257443180
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-18 14:31:45 -0300

    remove unused imports

--- register/src/test/java/com/example/RestaurantResourceTest.java
+++ register/src/test/java/com/example/RestaurantResourceTest.java
@@ -1,20 +1,14 @@
 package com.example;
 
-import entities.Restaurant;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
-import javax.transaction.Transactional;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 @QuarkusTestResource(RegisterTestLifecycleManager.class)
-@Transactional
 class RestaurantResourceTest {
 
     @Test
