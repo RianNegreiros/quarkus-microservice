577969e0603d68573240dd6d54edd7000b90c87a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-18 18:19:36 -0300

    create DishMapper

---
+++ register/src/main/java/mapper/DishMapper.java
@@ -0,0 +1,13 @@
+package mapper;
+
+import dto.DishDTO;
+import entities.Dish;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "cdi")
+public interface DishMapper {
+    DishMapper INSTANCE = Mappers.getMapper(DishMapper.class);
+
+    Dish toRestaurant(DishDTO dto);
+}
