2bd5e0f9c9c904af0db78cf89b88fc43a1604b25
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-20 17:30:15 -0300

    dish resource

--- marketplace/src/main/java/com/example/entity/Dish.java
+++ marketplace/src/main/java/com/example/entity/Dish.java
@@ -1,6 +1,16 @@
 package com.example.entity;
 
+import com.example.DTO.DishDTO;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
+
 import java.math.BigDecimal;
+import java.util.stream.StreamSupport;
 
 public class Dish {
 
@@ -13,4 +23,30 @@ public class Dish {
     public Restaurant restaurant;
 
     public BigDecimal price;
+
+    public static Multi<DishDTO> findAll(PgPool pgPool) {
+        Uni<RowSet<Row>> preparedQuery = pgPool.query("select * from dish").execute();
+        return unitToMulti(preparedQuery);
+    }
+
+    public static Multi<DishDTO> findAll(PgPool client, Long idRestaurant) {
+        Uni<RowSet<Row>> preparedQuery = client
+                .preparedQuery("SELECT * FROM dish where dish.restaurant_id = $1 ORDER BY nome ASC").execute(
+                        Tuple.of(idRestaurant));
+        return unitToMulti(preparedQuery);
+    }
+
+    private static Multi<DishDTO> unitToMulti(Uni<RowSet<Row>> queryResult) {
+        return queryResult.onItem()
+                .transformToMulti(set -> Multi.createFrom().items(() -> {
+                    return StreamSupport.stream(set.spliterator(), false);
+                }))
+                .onItem().transform(DishDTO::from);
+    }
+
+    public static Uni<DishDTO> findById(PgPool client, Long id) {
+        return client.preparedQuery("SELECT * FROM dish WHERE id = $1").execute(Tuple.of(id))
+                .map(RowSet::iterator)
+                .map(iterator -> iterator.hasNext() ? DishDTO.from(iterator.next()) : null);
+    }
 }
