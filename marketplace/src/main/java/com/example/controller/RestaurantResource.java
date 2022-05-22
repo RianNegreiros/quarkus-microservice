d633cda9c65a5e14222f601e59201c1a4d7e409c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-22 11:15:09 -0300

    remove unsed imports

--- marketplace/src/main/java/com/example/controller/RestaurantResource.java
+++ marketplace/src/main/java/com/example/controller/RestaurantResource.java
@@ -2,7 +2,6 @@ package com.example.controller;
 
 
 import com.example.DTO.DishDTO;
-import com.example.DTO.RestaurantDTO;
 import com.example.entity.Dish;
 import com.example.entity.Restaurant;
 import io.smallrye.mutiny.Multi;
